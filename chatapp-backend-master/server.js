e1b40d0b88ee96818b677f3d27ffe10329cc35c4
Rahul Aher <rahul.aher@morningstar.com>
2024-09-16 14:44:47 +0530

    cors error

--- chatapp-backend-master/server.js
+++ chatapp-backend-master/server.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
   }
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, OPTIONS");
   next();
 });
 
